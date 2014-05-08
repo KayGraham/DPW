38059d11afe35b9dbe0e544e2fe415305ea60d49
KayGraham <KayGraham3@fullsail.edu>
2014-05-08 13:41:09 -0400

    Classes 2

--- classes-html/main.py
+++ classes-html/main.py
@@ -1,24 +1,17 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+'''
+Kayla Robinson
+5/8/2014
+DPW
+Classes 2
+'''
 import webapp2
+from pages import Page #from package import Class
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = Page()
+        p.body = "Miss Piggy like Kermit De Frog!"
+        self.response.write(p.print_out())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
