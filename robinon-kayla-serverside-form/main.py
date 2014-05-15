cfc96ad1f08da6eb3c29e5ae1ae2e6ef1099a29c
KayGraham <KayGraham3@fullsail.edu>
2014-05-15 17:16:23 -0400

    Simple Form

---
+++ robinon-kayla-serverside-form/main.py
@@ -0,0 +1,16 @@
+'''
+Kayla Robinson
+5/15/20014
+DPW
+Simple Form
+'''
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
