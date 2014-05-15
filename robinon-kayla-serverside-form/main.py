55ee0dc9b12d046fe98449074411faf999077839
KayGraham <KayGraham3@fullsail.edu>
2014-05-15 17:26:11 -0400

    Check Box

--- robinon-kayla-serverside-form/main.py
+++ robinon-kayla-serverside-form/main.py
@@ -22,7 +22,10 @@ class MainHandler(webapp2.RequestHandler):
         <form method="GET">
             <label>Name: </label><input type="text" name="user"/>
             <label>Email: </label><input type="text" name="email"/>
-
+            <label>Age: </label><input type="checkbox" name="age" value="18-29">18-29<br>
+            <input type="checkbox" name="age" value="30-39">30-39<br>
+            <input type="checkbox" name="age" value="40-49">40-49<br>
+            <input type="checkbox" name="age" value="50+">50+
             <label>City: </label><input type="text" name="city"/>
         '''
         page_close = '''
