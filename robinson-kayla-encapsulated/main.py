7ac96a72461319f0c31bf5a6f59537914e7bd8a4
KayGraham <KayGraham3@fullsail.edu>
2014-05-15 00:23:39 -0400

    Monthly Property

--- robinson-kayla-encapsulated/main.py
+++ robinson-kayla-encapsulated/main.py
@@ -8,17 +8,35 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        option1 = Rental()
+        option1.name = "Rental Property 1"
+        option1.mortgage_amount = 30000
+        option1.rent = 400
+        option1.calc_monhly_profit()
+        option1.calc_annual_profit()
+        self.reponse.write("monthly profit" + str(option1.monthy_profit))
 
 #Rental property class object
 class Rental(object):
     def __init__(self):
         #attributes
-        self.location = ""
+        self.name = ""
         self.mortgage_amount = 0
         self.rent = 0
-        self.monthly_profit = 0
-        self.annual_profit = 0
+        self.__monthly_profit = 0
+        self.__annual_profit = 0
+
+    @property
+    def monthly_profit(self):
+        return self.__monthly_profit
+
+    @monthly_profit.setter
+    def monthly_profit(self, new_monthly_profit):
+        self.__monthly_profit = new_monthly_profit
+
+    def calc_monthly_profit(self):
+        #calculate monthly profit
+        self.__monthly_profit = ((self.mortgage_amount / 12) - self.rent)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
