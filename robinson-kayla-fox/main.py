c79407d3f929b9dc063efc9aa0526ff87b2f68a4
KayGraham <KayGraham3@fullsail.edu>
2014-05-22 13:17:41 -0400

    Clases

--- robinson-kayla-fox/main.py
+++ robinson-kayla-fox/main.py
@@ -62,7 +62,7 @@ class Animal(object):
     def animal_sound(self):
         sound = ''
 
-class RedFox(Page):
+class RedFox(Animal):
     def __init__(self):
         super(RedFox, self).__init__()
         self.animal_name = 'Red Fox'
@@ -76,55 +76,42 @@ class RedFox(Page):
         self.animal_habitat = 'Woods, prairies and farmlands'
         self.animal_geolocation = 'Europe, Asia and North America'
 
-
     def animal_sound(self):
         sound = 'Wa-pa-pa-pa-pa-pa-pow'
 
-class Zebra(Page):
+class Zebra(Animal):
     def __init__(self):
         super(Zebra, self).__init__()
-        self._zebra_name = 'Zebra'
-        self._zebra_phylum = 'Chordata'
-        self._zebra_class = 'Mammalia'
-        self._zebra_order = 'Perissodactyla'
-        self._zebra_family = 'Equidae'
-        self._zebra_genus = 'Equus'
-        self._zebra_image = 'images/zebra.png'
-        self._zebra_lifespan = '20-30 years'
-        self._zebra_habitat = 'Grassy Plains, open woodlands and grassy mountain slopes'
-        self._zebra_geolocation = 'Africa'
-        self._zebra = ''
-        self._zebra_info = [self._zebra_name, self._zebra_phylum, self._zebra_class, self._zebra_order, self._zebra_family, self._zebra_genus, self._zebra_image, self._zebra_lifespan, self._zebra_habitat, self._zebra_geolocation]
+        self.animal_name = 'Zebra'
+        self.animal_phylum = 'Chordata'
+        self.animal_class = 'Mammalia'
+        self.animal_order = 'Perissodactyla'
+        self.animal_family = 'Equidae'
+        self.animal_genus = 'Equus'
+        self.animal_image = 'images/zebra.png'
+        self.animal_lifespan = '20-30 years'
+        self.animal_habitat = 'Grassy Plains, open woodlands and grassy mountain slopes'
+        self.animal_geolocation = 'Africa'
 
-        self._zebra += '<h2>' + self._zebra_info[0] + '</p><img src=" ' + self._zebra_info[6] + '" /></h2>' + '<p><strong>Phylum:</strong> ' + self._zebra_info[1] + '</p><p><strong>Class:</strong> ' + self._zebra_info[2] + '</p><p><strong>Order:</strong> ' + self._zebra_info[3] + '</p><p><strong>Family:</strong> ' + self._zebra_info[4] + '</p><p><strong>Genus:</strong> ' + self._zebra_info[5] + '" </p><p><strong>Average Lifespan:</strong> ' + self._zebra_info[7] + '</p><p><strong>Habitat:</strong> ' + self._zebra_info[8] + '</p><p><strong>Geolocation:</strong> ' + self._zebra_info[9]
+    def animal_sound(self):
+        sound = 'Yip'
 
-        print self._zebra
-
-    def print_out(self):
-        return self._head + self._body + self._zebra + '<div id="say"><h3>What does the Zebra say?</h3><p>"Yip"</p></div>' + self._close
-
-class Panda(Page):
+class Panda(Animal):
     def __init__(self):
         super(Panda, self).__init__()
-        self._panda_name = 'Giant Panda'
-        self._panda_phylum = 'Chordata'
-        self._panda_class = 'Mammalia'
-        self._panda_order = 'Carnivora'
-        self._panda_family = 'Ursidae'
-        self._panda_genus = 'Ailuropoda'
-        self._panda_image = 'images/panda.png'
-        self._panda_lifespan = '20 years'
-        self._panda_habitat = 'Bamboo Forests'
-        self._panda_geolocation = 'Western China'
-        self._panda = ''
-        self._panda_info = [self._panda_name, self._panda_phylum, self._panda_class, self._panda_order, self._panda_family, self._panda_genus, self._panda_image, self._panda_lifespan, self._panda_habitat, self._panda_geolocation]
+        self.animal_name = 'Giant Panda'
+        self.animal_phylum = 'Chordata'
+        self.animal_class = 'Mammalia'
+        self.animal_order = 'Carnivora'
+        self.animal_family = 'Ursidae'
+        self.animal_genus = 'Ailuropoda'
+        self.animal_image = 'images/panda.png'
+        self.animal_lifespan = '20 years'
+        self.animal_habitat = 'Bamboo Forests'
+        self.animal_geolocation = 'Western China'
 
-        self._panda += '<h2>' + self._panda_info[0] + '</p><img src=" ' + self._panda_info[6] + '" /></h2>' + '<p><strong>Phylum:</strong> ' + self._panda_info[1] + '</p><p><strong>Class:</strong> ' + self._panda_info[2] + '</p><p><strong>Order:</strong> ' + self._panda_info[3] + '</p><p><strong>Family:</strong> ' + self._panda_info[4] + '</p><p><strong>Genus:</strong> ' + self._panda_info[5] + '" </p><p><strong>Average Lifespan:</strong> ' + self._panda_info[7] + '</p><p><strong>Habitat:</strong> ' + self._panda_info[8] + '</p><p><strong>Geolocation:</strong> ' + self._panda_info[9]
-
-        print self._panda
-
-    def print_out(self):
-        return self._head + self._body + self._panda + '<div id="say"><h3>What does the Giant Panda say?</h3><p>"bleat"</p></div>' + self._close
+    def animal_sound(self):
+        sound = 'bleat'
 
 class Select(Page):
     def __init__(self):
