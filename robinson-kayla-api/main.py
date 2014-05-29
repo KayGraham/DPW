4c9d06757a482ff678b5725dc35b05d40c1ba7dd
KayGraham <KayGraham3@fullsail.edu>
2014-05-28 23:37:54 -0400

    FormPage Subclass

--- robinson-kayla-api/main.py
+++ robinson-kayla-api/main.py
@@ -141,7 +141,31 @@ class Page(object):
     def print_out(self):
         return self._head + self._body + self._close
 
+class FormPage(Page):
+    def __init__(self):
+        super(FormPage, self).__init__()
+        self._form_open = '<form method="GET">'
+        self._form_close = '</form>'
+        self.__inputs = []
+        self._form_inputs = ''
 
+    @property
+    def inputs(self):
+        pass
+
+    @inputs.setter
+    def inputs(self, arr):
+        self.__inputs = arr
+        for item in arr:
+            self._form_inputs += '<input type="' + item[1] + '" name="' + item[0]
+            try:
+                self._form_inputs += '" placeholder="' + item[2] + '" required/>'
+
+            except:
+                self._form_inputs += '" />'
+
+    def print_out(self):
+        return self._head + self._form_open + self._form_inputs + self._form_close + self._body + self._close
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
