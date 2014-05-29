3903c62daf9ac61d30cc569e4acd1d6ac0947fb0
KayGraham <KayGraham3@fullsail.edu>
2014-05-28 23:01:53 -0400

    Mainhandler

--- robinson-kayla-api/main.py
+++ robinson-kayla-api/main.py
@@ -10,7 +10,32 @@ import json
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = FormPage()
-        p.inputs = [['category', 'text', 'category'],['city', 'text', 'location'],['state', 'text', 'region'],['zip code', 'text', 'postal_code'],['Submit', 'submit']]
+        p.inputs = [['category', 'text', 'category'],['city', 'text', 'locality'],['state', 'text', 'region'],['Submit', 'submit']]
+
+        if self.request.GET:
+            #Create Venue Model
+            vm = VenueModel()
+
+            #Sends category from url to the model
+            vm.category = self.request.GET['category']
+            #Sends city from url to the model
+            vm.category = self.request.GET['locality']
+            #Sends state from url to the model
+            vm.category = self.request.GET['region']
+
+            #Connect to API
+            vm.callApi()
+
+            #Create view
+            vv = VenueView()
+
+            #Take data from model and sends to view
+            vv.vdos = vm.dos
+
+            p._body = vv.content
+
+            self.response.write(p.print_out())
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
